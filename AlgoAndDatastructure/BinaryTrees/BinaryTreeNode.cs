26925fc03c25d3b1c646e33761dce60c8a496d6a
collins Anieze <chigoziecollins434@gmail.com>
2022-03-28 22:40:01 +0100

    Add project files.

---
+++ AlgoAndDatastructure/BinaryTrees/BinaryTreeNode.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoAndDatastructure.BinaryTrees
+{
+    public class BinaryTreeNode
+    {
+        public char Value { get; set; }
+        public BinaryTreeNode Left { get; set; }
+        public BinaryTreeNode Right { get; set; }
+
+        public BinaryTreeNode(char value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public static List<char> DepthFirstValues(BinaryTreeNode rootNode)
+        {
+            if(rootNode == null)
+            {
+                return new List<char>();
+            }
+
+            var rtnList = new List<char>();
+            var stack = new Stack<BinaryTreeNode>();
+            stack.Push(rootNode);
+
+            while(stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+                rtnList.Add(currentNode.Value);
+  
+
+                if(currentNode.Right != null) stack.Push(currentNode.Right);
+                if(currentNode.Left != null) stack.Push(currentNode.Left);
+            }
+
+            return rtnList;
+        }
+
+        public static List<char> DepthFirstValuesRecursion(BinaryTreeNode rootNode)
+        {
+            if (rootNode == null)
+            {
+                return new List<char>();
+            }
+
+            var rtnList = new List<char>();
+
+            var rightValues = DepthFirstValuesRecursion(rootNode.Right);
+            var leftValues = DepthFirstValuesRecursion(rootNode.Left);
+
+            rtnList.Add(rootNode.Value);
+            rtnList.AddRange(leftValues);
+            rtnList.AddRange(rightValues);
+            return rtnList;
+        }
+
+        public static List<char> BreadthFirstValues(BinaryTreeNode rootNode)
+        {
+            if (rootNode == null)
+            {
+                return new List<char>();
+            }
+
+            var rtnList = new List<char>();
+
+            var queue = new Queue<BinaryTreeNode>();
+            queue.Enqueue(rootNode);
+
+            while(queue.Count > 0)
+            {
+                BinaryTreeNode currentNode = queue.Dequeue();
+                rtnList.Add(currentNode.Value);
+
+                if(currentNode.Left != null)
+                {
+                    queue.Enqueue(currentNode.Left);
+                }
+
+                if(currentNode.Right != null)
+                {
+                    queue.Enqueue(currentNode.Right);
+                }
+            }
+
+           
+            return rtnList;
+        }
+    }
+}
---
+++ AlgoAndDatastructure/Program.cs
@@ -0,0 +1,38 @@
+﻿using AlgoAndDatastructure.BinaryTrees;
+using System;
+using System.Text.Json;
+
+namespace AlgoAndDatastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(JsonSerializer.Serialize(BinaryTree.DepthFirstValues(SetUpTree())));
+            Console.WriteLine(JsonSerializer.Serialize(BinaryTree.DepthFirstValuesRecursion(SetUpTree())));
+            Console.WriteLine(JsonSerializer.Serialize(BinaryTree.BreadthFirstValues(SetUpTree())));
+            Console.ReadLine();
+        }
+
+        static BinaryTreeNode SetUpTree()
+        {
+            var a = new BinaryTreeNode('a');
+            var b = new BinaryTreeNode('b');
+            var c = new BinaryTreeNode('c');
+            var d = new BinaryTreeNode('d');
+            var e = new BinaryTreeNode('e');
+            var f = new BinaryTreeNode('f');
+
+            a.Left = b;
+            a.Right = c;
+            b.Left = d;
+            b.Right = e;
+            c.Right = f;
+
+            return a;
+        }
+    }
+
+
+}

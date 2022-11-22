c2200eab172182c13aae8e09627a8ed0f346b2bd
collins Anieze <chigoziecollins434@gmail.com>
2022-11-22 10:40:08 +0100

    Half completion of fruits in a basket algorithm

--- AlgoAndDatastructure/GrokkingAlgorithmByA/SlidingWindow.cs
+++ AlgoAndDatastructure/GrokkingAlgorithmByA/SlidingWindow.cs
@@ -64,26 +64,166 @@ namespace AlgoAndDatastructure.GrokkingAlgorithmByA
             
         }
 
-        public static int SmallestSubArrayWithAGivenSum(int[] arr, int positiveNumber)
+        //public static int SmallestSubArrayWithAGivenSum(int[] arr, int positiveNumber)
+        //{
+        //    if(arr.Length == 0 || arr == null || positiveNumber <= 0)
+        //    {
+        //        return 0;
+        //    }
+
+        //    int minimumLength = int.MaxValue;
+        //    int arrLength = arr.Length;
+        //    int windowStart = 0;
+        //    int windowSum = 0;
+
+        //    for (int windowEnd = 0; windowEnd < arrLength; windowEnd++)
+        //    {
+        //        windowSum += arr[windowEnd];
+
+        //        while(windowSum >= positiveNumber)
+        //        {
+        //            minimumLength = Math.Min(minimumLength, windowEnd - windowStart + 1);
+        //            windowSum -= arr[windowStart];
+        //            windowStart++;
+        //        }
+        //    }
+
+        //    return minimumLength == int.MaxValue ? 0 : minimumLength;
+        //}
+
+        //public static int LongestSubStringWithKDistinctCharacters(string str, int k)
+        //{
+        //    if(string.IsNullOrEmpty(str) || str.Length < k)
+        //    {
+        //        return 0;
+        //    }
+
+        //    int strLength = str.Length;
+        //    int maxLength = int.MinValue;
+        //    int windowStart = 0;
+
+        //    Dictionary<char, int> dict = new Dictionary<char, int>();
+
+
+        //    for(int windowEnd = 0; windowEnd < strLength; windowEnd++)
+        //    {
+               
+        //        if (dict.ContainsKey(str[windowEnd]))
+        //        {
+        //            char currentChar = str[windowEnd];
+        //            dict[currentChar] = dict[currentChar] + 1;
+        //        }
+        //        else
+        //        {
+        //            dict.Add(str[windowEnd], 1);
+        //        }
+
+        //        while (dict.Count > k)
+        //        {
+        //            char key = str[windowStart];
+        //            dict.TryGetValue(key, out int value);
+
+                
+        //            if (value == 1)
+        //            {
+        //                dict.Remove(key);
+        //            }
+        //            else
+        //            {
+        //                dict[key] = dict[key] - 1;
+        //            }
+                                     
+        //            windowStart++;
+                    
+        //        }
+
+        //        maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+
+        //    }
+
+        //    return maxLength;
+        //}
+   
+        //public static int LengthOfLongestSubstringWithNoRepeatingCharacters(string str)
+        //{
+        //    if (string.IsNullOrEmpty(str))
+        //    {
+        //        throw new ArgumentNullException();
+        //    }
+        //    //aabccbb
+
+        //    Dictionary<char, int> dict = new Dictionary<char, int>();
+        //    int strLength = str.Length;
+        //    int maxLength = int.MinValue;
+        //    int windowStart = 0;
+                         
+        //    for(int windowEnd = 0; windowEnd < strLength; windowEnd++)
+        //    {
+        //        char key = str[windowEnd];
+
+        //        if (dict.ContainsKey(key))
+        //        {
+        //            dict[key] = dict[key] + 1;                   
+        //        }
+        //        else
+        //        {
+        //            dict.Add(str[windowEnd], 1);
+        //        }
+
+
+        //        maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+        //    }
+        //}
+    
+        public static int MaxSumSubArrayOfSizeK(int[] nums, int size)
         {
-            if(arr.Length == 0 || arr == null || positiveNumber <= 0)
+            int len = nums.Length;
+
+            if(len < size || nums == null || len == 0)
+            {
+                return 0;
+            }
+
+            int windowStart = 0;
+            int maxSum = 0;
+            int windowSum = 0;
+
+            for(int windowEnd = 0; windowEnd < len; windowEnd++)
+            {
+                windowSum += nums[windowEnd];
+
+                if(windowEnd >= size - 1)
+                {
+                    maxSum = Math.Max(maxSum, windowSum);
+                    windowSum -= nums[windowStart];
+                    windowStart++;
+                }
+            }
+
+            return maxSum;
+        }
+   
+        public static int SmallestSubArrayWithAGivenSum(int[] nums, int number)
+        {
+            int length = nums.Length;
+
+            if(nums == null || length == 0 || number < 1)
             {
                 return 0;
             }
 
             int minimumLength = int.MaxValue;
-            int arrLength = arr.Length;
             int windowStart = 0;
             int windowSum = 0;
 
-            for (int windowEnd = 0; windowEnd < arrLength; windowEnd++)
+            for (int windowEnd = 0; windowEnd < length; windowEnd++)
             {
-                windowSum += arr[windowEnd];
+                windowSum += nums[windowEnd];
 
-                while(windowSum >= positiveNumber)
+                while(windowSum >= number)
                 {
                     minimumLength = Math.Min(minimumLength, windowEnd - windowStart + 1);
-                    windowSum -= arr[windowStart];
+                    windowSum -= nums[windowStart];
                     windowStart++;
                 }
             }
@@ -93,55 +233,98 @@ namespace AlgoAndDatastructure.GrokkingAlgorithmByA
 
         public static int LongestSubStringWithKDistinctCharacters(string str, int k)
         {
-            if(string.IsNullOrEmpty(str) || str.Length < k)
+            int length = str.Length;
+
+            if(string.IsNullOrWhiteSpace(str) || length < k)
             {
-                return 0;
+                return -1;
             }
 
-            int strLength = str.Length;
-            int maxLength = int.MinValue;
+            int maximumLength = 0;
             int windowStart = 0;
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
 
-            Dictionary<char, int> dict = new Dictionary<char, int>();
-
-
-            for(int windowEnd = 0; windowEnd < strLength; windowEnd++)
+            for(int windowEnd = 0; windowEnd < length; windowEnd++)
             {
-               
-                if (dict.ContainsKey(str[windowEnd]))
+                char currentChar = str[windowEnd];
+
+                if (dictionary.ContainsKey(currentChar))
                 {
-                    char currentChar = str[windowEnd];
-                    dict[currentChar] = dict[currentChar] + 1;
+                    dictionary[currentChar] += 1;
                 }
                 else
                 {
-                    dict.Add(str[windowEnd], 1);
+                    dictionary.Add(currentChar, 1);
                 }
-
-                while (dict.Count > k)
+                //araaci
+                while (dictionary.Count > k)
                 {
                     char key = str[windowStart];
-                    dict.TryGetValue(key, out int value);
 
-                
-                    if (value == 1)
+                    if (dictionary[key] == 1)
                     {
-                        dict.Remove(key);
+                        dictionary.Remove(key);
                     }
                     else
                     {
-                        dict[key] = dict[key] - 1;
+                        dictionary[key] -= 1;
                     }
-                                     
+
                     windowStart++;
-                    
                 }
 
-                maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
-
+                maximumLength = Math.Max(maximumLength, windowEnd - windowStart + 1);
             }
 
-            return maxLength;
+            return maximumLength;
+        }
+   
+        //public static int MaximumNumberOfFruitsInABasket(char[] fruitTree)
+        //{
+        //    int length = fruitTree.Length;
+
+        //    if(fruitTree == null || length == 0)
+        //    {
+        //        return 0;
+        //    }
+
+        //    int maxNumberOfFruits = 0;
+        //    int windowStart = 0;
+        //    Dictionary<char, int> fruitMap = new Dictionary<char, int>();
+
+        //    for(int windowEnd = 0; windowEnd < length; windowEnd++)
+        //    {
+        //        char fruit = fruitTree[windowEnd];
+
+        //        if (fruitMap.ContainsKey(fruit))
+        //        {
+        //            fruitMap[fruit] += 1;
+        //        }
+        //        else
+        //        {
+        //            fruitMap.Add(fruit, 1);
+        //        }
+
+        //        while(fruitMap.Count > 2)
+        //        {
+        //            maxNumberOfFruits = Math.Max(maxNumberOfFruits, windowEnd - windowStart + 1);
+
+        //        }
+
+        //        maxNumberOfFruits = Math.Max(maxNumberOfFruits, GetTotalFruitCount(fruitMap));
+        //    }
+        //}
+
+        private static int GetTotalFruitCount(Dictionary<char, int> fruitMap)
+        {
+            int sum = 0;
+
+            foreach(KeyValuePair<char, int> kvp in fruitMap)
+            {
+                sum += kvp.Value;
+            }
+
+            return sum;
         }
     }
 }
